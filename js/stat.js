d42113d7e500729db58e80e15d7e652da05493af
Anna <anna_cuba@mail.ru>
2020-05-30 20:52:42 +0300

    Добавлен финальный текст на облако

--- js/stat.js
+++ js/stat.js
@@ -9,4 +9,9 @@ var renderCloud = function(ctx, x, y, color){
 window.renderStatistics = function(ctx) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
+
+ ctx.font = '16px PT Mono';
+ ctx.fillStyle = '#000';
+ ctx.fillText('Ура вы победили!', 120, 40);
+ ctx.fillText('Список результатов:', 120, 60);
 };
