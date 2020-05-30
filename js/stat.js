f3526af404548d0f2cd6b74008c18ff0c033131b
Anna <anna_cuba@mail.ru>
2020-05-30 19:55:58 +0300

    Создан js файл для статистики

---
+++ js/stat.js
@@ -0,0 +1,8 @@
+'use strict';
+
+window.renderStatistics = function(ctx) {
+  ctx.fillStyle(rgba(0, 0, 0, 0.7));
+  ctx.fillRect(110, 20, 270, 420);
+  ctx.fillStyle = "white";
+  ctx.fillRect(100, 10, 270, 420);
+};
