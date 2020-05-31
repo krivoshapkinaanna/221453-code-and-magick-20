e18cc60185fc525070c3bff65e84318a437e5b0f
Anna <anna_cuba@mail.ru>
2020-05-31 22:22:06 +0300

    Высота колонок считается относительно макс.высоты колонки

--- js/stat.js
+++ js/stat.js
@@ -15,6 +15,16 @@ var renderCloud = function(ctx, x, y, color){
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
+
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    };
+  };
+  return maxElement;
+};
 window.renderStatistics = function(ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
@@ -25,9 +35,11 @@ window.renderStatistics = function(ctx, players, times) {
  ctx.fillText('Список результатов:', CLOUD_X + GAP, 60);
  ctx.fillStyle = '#000';
 
+var maxTime = getMaxElement(times);
 
 for (var i = 0; i < players.length; i++) {
   ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y_BOTTOM - GAP);
-  ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y_BOTTOM - GAP - FONT_GAP, BAR_WIDTH, - BAR_HEIGHT);
+  ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y_BOTTOM - GAP - FONT_GAP, BAR_WIDTH, - (BAR_HEIGHT * times[i] / maxTime));
+  ctx.fillText('times[i]', CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y_BOTTOM - GAP - (BAR_HEIGHT * times[i] / maxTime) - FONT_GAP); //пока непонятно как настроить отступы//
 };
 };
