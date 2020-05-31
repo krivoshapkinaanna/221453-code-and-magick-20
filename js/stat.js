55c730777e00f42919341fd6d5669a916c9f86bd
Anna <anna_cuba@mail.ru>
2020-05-31 21:12:28 +0300

    Добавлены константы и одна колонка

--- js/stat.js
+++ js/stat.js
@@ -2,16 +2,32 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var CLOUD_Y_BOTTOM = CLOUD_Y + CLOUD_HEIGHT;
+var GAP = 20;
+var BAR_GAP = 50;
+var FONT_GAP = 20;
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+
 var renderCloud = function(ctx, x, y, color){
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 window.renderStatistics = function(ctx) {
-  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, 100, 10, '#fff');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
  ctx.font = '16px PT Mono';
  ctx.fillStyle = '#000';
- ctx.fillText('Ура вы победили!', 120, 40);
- ctx.fillText('Список результатов:', 120, 60);
-};
+ ctx.fillText('Ура вы победили!', CLOUD_X + GAP, 40);
+ ctx.fillText('Список результатов:', CLOUD_X + GAP, 60);
+ ctx.fillStyle = '#000';
+ ctx.fillText('Вы', CLOUD_X + BAR_GAP, CLOUD_Y_BOTTOM - GAP);
+ctx.fillRect(CLOUD_X + BAR_GAP, CLOUD_Y_BOTTOM - GAP - FONT_GAP, BAR_WIDTH, - BAR_HEIGHT);
+
+
+
+
+
