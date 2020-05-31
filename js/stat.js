9246f7d7a2d32d553266829418bbbe89215ae3c2
Anna <anna_cuba@mail.ru>
2020-05-31 21:47:38 +0300

    Прописаны цикл и массив для выведения имен и колонок

--- js/stat.js
+++ js/stat.js
@@ -15,7 +15,7 @@ var renderCloud = function(ctx, x, y, color){
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
-window.renderStatistics = function(ctx) {
+window.renderStatistics = function(ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
@@ -24,10 +24,10 @@ window.renderStatistics = function(ctx) {
  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, 40);
  ctx.fillText('Список результатов:', CLOUD_X + GAP, 60);
  ctx.fillStyle = '#000';
- ctx.fillText('Вы', CLOUD_X + BAR_GAP, CLOUD_Y_BOTTOM - GAP);
-ctx.fillRect(CLOUD_X + BAR_GAP, CLOUD_Y_BOTTOM - GAP - FONT_GAP, BAR_WIDTH, - BAR_HEIGHT);
 
 
-
-
-
+for (var i = 0; i < players.length; i++) {
+  ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y_BOTTOM - GAP);
+  ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y_BOTTOM - GAP - FONT_GAP, BAR_WIDTH, - BAR_HEIGHT);
+};
+};
