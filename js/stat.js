1d6c1c10bc1d1c4f9259d82a27f7def7f73d1ef7
Anna <anna_cuba@mail.ru>
2020-06-03 20:05:13 +0300

    Добавлен цвет колонок, в цикл внесена переменная

--- js/stat.js
+++ js/stat.js
@@ -11,35 +11,45 @@ var FONT_GAP = 20;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 
-var renderCloud = function(ctx, x, y, color){
+
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
-    };
-  };
+    }
+  }
   return maxElement;
 };
-window.renderStatistics = function(ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)'); //Как быть здесь со значением У?
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
- ctx.font = '16px PT Mono';
- ctx.fillStyle = '#000';
- ctx.fillText('Ура вы победили!', CLOUD_X + GAP, 40);
- ctx.fillText('Список результатов:', CLOUD_X + GAP, 60);
- ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = '#000';
 
-var maxTime = getMaxElement(times);
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, 40);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, 60);
 
-for (var i = 0; i < players.length; i++) {
-  ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y_BOTTOM - GAP);
-  ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y_BOTTOM - GAP - (BAR_HEIGHT * Math.round(times[i]) / maxTime) - FONT_GAP - GAP); //пока непонятно как настроить отступы//
-  ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y_BOTTOM - GAP - FONT_GAP, BAR_WIDTH, - (BAR_HEIGHT * Math.round(times[i]) / maxTime)); //можно ли по-другому оформить округление?
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = '#000';
+    var barBottomX = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
+    ctx.fillText(players[i], barBottomX, CLOUD_Y_BOTTOM - GAP);
+    ctx.fillText(times[i].toFixed(1), barBottomX, CLOUD_Y_BOTTOM - GAP - (BAR_HEIGHT * times[i] / maxTime) - FONT_GAP - GAP); // пока непонятно как настроить отступы//
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'hsl(228, 100%, ' + Math.floor(Math.random() * 100) + '%)';
+    }
+    ctx.fillRect(barBottomX, CLOUD_Y_BOTTOM - GAP - FONT_GAP, BAR_WIDTH, -(BAR_HEIGHT * times[i] / maxTime));
+  }
 };
-};
+
+
