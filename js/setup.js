9e1b4b2018cca538cae500ccfebb1aabf417d894
Anna <anna_cuba@mail.ru>
2020-06-15 22:35:30 +0300

    Конечный вариант отправки формы, добавлены  валидация имени, обработчик смены фаербола, пальто и глаз

--- js/setup.js
+++ js/setup.js
@@ -5,13 +5,15 @@ var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 var TOTALWIZARDS = 4;
 var FIREBALL_WRAP = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+var MIN_NAME_LENGTH = 2;
+var MAX_NAME_LENGTH = 25;
 
 var setupOpen = document.querySelector('.setup-open');
 var setup = document.querySelector('.setup');
 var setupClose = setup.querySelector('.setup-close');
 var setupForm = setup.querySelector('.setup-wizard-form');
 
-
+// Задали переменную обработчика Escape
 var onPopupEscPress = function (evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
@@ -19,6 +21,7 @@ var onPopupEscPress = function (evt) {
   }
 };
 
+// Объявили функцию открытия и закрытия.
 var openPopup = function () {
   setup.classList.remove('hidden');
   document.addEventListener('keydown', onPopupEscPress);
@@ -29,6 +32,7 @@ var closePopup = function () {
   document.removeEventListener('keydown', onPopupEscPress);
 };
 
+// Повесили обработчик на открытие и закрытие. (Согласно лекции по критериям обработчики нужно отделять от функций, которые несут какую-то логику)
 setupOpen.addEventListener('click', function () {
   openPopup();
 });
@@ -39,10 +43,6 @@ setupOpen.addEventListener('keydown', function (evt) {
   }
 });
 
-setupForm.addEventListener('submit', function () {
-  closePopup();
-});
-
 setupClose.addEventListener('click', function () {
   closePopup();
 });
@@ -50,6 +50,12 @@ setupClose.addEventListener('keydown', function (evt) {
   if (evt.key === 'Enter') {
     closePopup();
   }
+});
+
+// Добавлен обработчик на форму. (Пока оставили в таком виде, позже добавить evt в функцию.
+setupForm.addEventListener('submit', function () {
+  // evt.preventDefault();
+  closePopup();
 });
 
 document.querySelector('.setup-similar').classList.remove('hidden');
@@ -104,4 +110,52 @@ var renderWizards = function () {
   similarListElement.appendChild(fragment);
 };
 renderWizards();
+
+// Смена цвета фаербола по клику
+var setupFireball = setup.querySelector('.setup-fireball-wrap');
+setupFireball.addEventListener('click', function () {
+  var fireballColor = FIREBALL_WRAP[getRandom(0, FIREBALL_WRAP.length - 1)];
+  setupFireball.style.background = fireballColor;
+  setupFireball.querySelector('input').value = fireballColor;
+});
+
+// Смена цвета пальто по клику
+var setupCoat = setup.querySelector('.setup-wizard .wizard-coat');
+setupCoat.addEventListener('click', function () {
+  var setupCoatColor = COAT_COLOR[getRandom(0, COAT_COLOR.length - 1)];
+  setupCoat.style.fill = setupCoatColor;
+  setupCoat.querySelector('input .coat-color').value = setupCoatColor;
+});
+
+// Смена цвета глаз по клику
+var setupEyes = setup.querySelector('.setup-wizard .wizard-eyes');
+setupEyes.addEventListener('click', function () {
+  var setupEyesColor = EYES_COLOR[getRandom(0, EYES_COLOR.length - 1)];
+  setupEyes.style.fill = setupEyesColor;
+  setupEyes.querySelector('input .eyes-color').value = setupEyesColor;
+});
+
+// Валидация формы имени
+var userNameInput = document.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+// Вывод кастомного сообщения при вводу в форму
+userNameInput.addEventListener('input', function () {
+  var valueLength = userNameInput.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) + ' симв.');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
 
